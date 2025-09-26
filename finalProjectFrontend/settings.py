2079d4b558e6cb5ea7422425236cb34df06d6df9
GabyDs <gabydsch2001@gmail.com>
2025-09-26 07:51:13 -0300

    Create basic view to receive an image

--- finalProjectFrontend/settings.py
+++ finalProjectFrontend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,7 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -116,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- monitor/views.py
+++ monitor/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, this is the home page of the monitor app.")
+
+def upload(request):
+    if request.method == 'POST' and request.FILES.get('file'):
+        image = request.FILES['file']
+        # Process the uploaded image
+        return HttpResponse("Image uploaded successfully.")
+    return HttpResponse("Upload an image.")
